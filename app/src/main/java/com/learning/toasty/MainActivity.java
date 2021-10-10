c34fea1433840682aca143afcc238b9691450593
joshi1996 <amitjoshi7597067843@gmail.com>
2021-10-10 22:33:20 +0530

    improve designing of toast layout

--- app/src/main/java/com/learning/toasty/MainActivity.java
+++ app/src/main/java/com/learning/toasty/MainActivity.java
@@ -3,6 +3,12 @@ package com.learning.toasty;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
--- toasty/src/main/java/com/learning/toasty/ToastJoshi.java
+++ toasty/src/main/java/com/learning/toasty/ToastJoshi.java
@@ -1,10 +1,28 @@
 package com.learning.toasty;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ToastJoshi {
-    public static void awesomeToast(Context context,String msg){
-        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    public static void awesomeToast(Context context, String msg){
+
+        final View toastLayout = ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE))
+                .inflate(R.layout.activity_toast, null);
+        final LinearLayout toastRoot = toastLayout.findViewById(R.id.toastLayout);
+        final TextView toastTextView = toastLayout.findViewById(R.id.toast);
+
+        toastTextView.setText(msg);
+        Toast toast = new Toast(context);
+        toast.setGravity(Gravity.BOTTOM, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(toastRoot);
+        toast.show();
     }
 }
