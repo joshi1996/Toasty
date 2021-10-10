3d39e58241162f6e787c8df9c9f86150ef3b72e6
joshi1996 <amitjoshi7597067843@gmail.com>
2021-10-10 19:30:23 +0530

    second update

--- toasty/src/main/java/com/learning/toasty/ToastJoshi.java
+++ toasty/src/main/java/com/learning/toasty/ToastJoshi.java
@@ -1,17 +1,10 @@
 package com.learning.toasty;
 
 import android.content.Context;
-import android.view.View;
 import android.widget.Toast;
 
 public class ToastJoshi {
     public static void awesomeToast(Context context,String msg){
-        Toast toast = Toast.makeText(context,
-                msg,
-                Toast.LENGTH_SHORT);
-
-        View toastView = toast.getView();
-        toastView.setBackgroundResource(R.color.design_default_color_primary_dark);
-        toast.show();
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
     }
 }
